2fa39095bd62a94d1ab1a9ddeaaa965bd612507d
mishamyrt <misha@myrt.co>
2025-03-16 13:07:56 +0300

    fix(linter): comment footer case fields for now
    Rules will be later

--- crates/commitfmt_linter/src/rules/footer/settings.rs
+++ crates/commitfmt_linter/src/rules/footer/settings.rs
@@ -1,11 +1,11 @@
-use crate::case::{TextCase, WordCase};
+// use crate::case::{TextCase, WordCase};
 
 #[derive(Debug, PartialEq, Default)]
 pub struct Settings {
     pub max_line_length: usize,
     pub max_length: usize,
     pub min_length: usize,
-    pub title_case: WordCase,
-    pub value_case: TextCase,
+    // pub title_case: WordCase,
+    // pub value_case: TextCase,
     pub required: Vec<Box<str>>,
 }
