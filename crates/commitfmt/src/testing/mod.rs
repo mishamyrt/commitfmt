80c82a81ec4b40235016a2c4894d206144e83be8
mishamyrt <misha@myrt.co>
2025-06-03 20:53:29 +0300

    chore: update rust to 1.88

--- crates/commitfmt/src/testing/mod.rs
+++ crates/commitfmt/src/testing/mod.rs
@@ -1,6 +1,5 @@
 use std::{io::Write, process::Stdio};
 
-#[allow(clippy::incompatible_msrv)]
 /// Create a pipe filled with the given string.
 pub fn pipe_from_string(input: &str) -> Stdio {
     let (reader, mut writer) = std::io::pipe().unwrap();
